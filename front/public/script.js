a520b1a5e314d49a27b70c99f11ff4a20b5a1d4a
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-03-19 16:22:48 +0100

    update 'deleteAnnonce()' (redirect to homepage)

--- front/public/script.js
+++ front/public/script.js
@@ -23,6 +23,8 @@ function deleteAnnonce(id) {
     }).then((response) => response.json())
         .then((json) => console.log(json))
         .catch((err) => console.error(err));
+
+    document.location.href='http://localhost:4000/';
 }
 
 function updateAnnonce(id) {
